50403d37bbd779d5d4829b83a91ec8383dca5372
Neri <nespinola@ospesalud.com.ar>
2022-02-02 17:47:32 -0300

    Se migra la logica de la consulta de productos desde ItemList al ItemListContainer

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,74 +1,11 @@
-import React, { useEffect, useState } from "react";
 import Item from "./Item";
 import { Row } from "react-bootstrap";
 
-const ItemList = () => {
-
-    const productosJson = [
-        {
-            id: 1,
-            title: "Producto 1",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",
-            price: 5000,
-            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
-            stock: 20
-        },
-        {
-            id: 2,
-            title: "Producto 2",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
-            price: 7500,
-            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
-            stock: 10
-        },
-        {
-            id: 3,
-            title: "Producto 3",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
-            price: 9000,
-            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
-            stock: 2
-        },
-        {
-            id: 4,
-            title: "Producto 4",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
-            price: 3200,
-            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
-            stock: 100
-        },
-        {
-            id: 5,
-            title: "Producto 5",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
-            price: 13300,
-            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
-            stock: 15
-        },
-    ]
-
-    const [productos, setProductos] = useState([]);
-
-	useEffect(() => {
-		const task = new Promise((resolve, reject) => {          
-            setTimeout(() => {
-                resolve();                
-            }, 2000);
-		});
-
-        task.then(() => {
-            setProductos(productosJson);
-        }, err => {
-            console.error(err);
-        })
-        .finally(() => {
-            console.info("Consulta de Productos finalizada");
-        });
-	}, []);    
+const ItemList = ({productos}) => {
 
 	return (
         <Row xs={1} md={4} className="g-4">
-            { productos.map( p =>             
+            { productos && productos.map( p =>             
                 <Item 
                     key={p.id} 
                     title={p.title} 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,10 +1,73 @@
+import { useEffect, useState } from "react";
 import { Alert } from "react-bootstrap";
 import ItemCount from "./ItemCount";
 import ItemList from "./ItemList";
 
 const ItemListContainer = ({saludo}) => {
 
-	const availableStock = 15;	
+	const availableStock = 15;
+
+    const productosJson = [
+        {
+            id: 1,
+            title: "Producto 1",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",
+            price: 5000,
+            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
+            stock: 20
+        },
+        {
+            id: 2,
+            title: "Producto 2",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
+            price: 7500,
+            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
+            stock: 10
+        },
+        {
+            id: 3,
+            title: "Producto 3",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
+            price: 9000,
+            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
+            stock: 2
+        },
+        {
+            id: 4,
+            title: "Producto 4",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
+            price: 3200,
+            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
+            stock: 100
+        },
+        {
+            id: 5,
+            title: "Producto 5",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus lacinia velit. Aliquam at purus mollis nisi porttitor dignissim. In tincidunt ligula in nibh aliquet placerat. Mauris feugiat nec tellus id fringilla. Cras mattis mauris non neque luctus, sed consectetur. ",            
+            price: 13300,
+            imageUrl: "https://radiogamasi.com/wp-content/uploads/2017/07/img_not_available.png",
+            stock: 15
+        },
+    ]
+
+    const [productos, setProductos] = useState([]);
+
+	useEffect(() => {
+		const task = new Promise((resolve, reject) => {          
+            setTimeout(() => {
+                resolve();                
+            }, 2000);
+		});
+
+        task.then(() => {
+            setProductos(productosJson);
+        }, err => {
+            console.error(err);
+        })
+        .finally(() => {
+            console.info("Consulta de Productos finalizada");
+        });
+	}, []);	
 
 	return (
 		<>
@@ -12,6 +75,7 @@ const ItemListContainer = ({saludo}) => {
 				<Alert.Heading>{saludo}</Alert.Heading>
 				<hr />
 				<ItemCount stock={availableStock} />
+				<ItemList productos={productos} />
 			</Alert>
 			<ItemList />
 		</>
