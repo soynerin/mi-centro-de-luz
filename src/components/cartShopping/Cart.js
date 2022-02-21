c2467ff3824e74607129bc3235469c3009218cc1
soyNerin <neri.agustin.es@outlook.com>
2022-02-20 23:53:38 -0300

    (feat) Nueva funcionalidad al Carrito de Compras
    * Se agrega el componente Cart
    * Se muestra el desglose de los items y el precio total

---
+++ src/components/cartShopping/Cart.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Cart = ({ cartItems }) => {
+	const totalPriceCart = cartItems.reduce(
+		(n, { producto, productQauntityCart }) => n + producto["price"] * productQauntityCart,
+		0
+	);
+
+	return (
+		<div className="p-5 min-vh-100" style={{ backgroundColor: "#F3F2FC" }}>
+			<h1 className="text-uppercase fs-3 fw-light mb-5"> Carrito </h1>
+			<ol className="list-group">
+				{cartItems &&
+					cartItems.map((item) => (
+						<li className="list-group-item d-flex justify-content-between align-items-start shadow-sm p-3 mb-5 bg-body rounded">
+							<div className="ms-2 me-auto">
+								<div className="fw-bold mb-4">{item["producto"]["title"]}</div>
+								<div className="d-flex w-100 justify-content-between">
+									<span className="w-75">{item["producto"]["description"]}</span>
+									<span className="w-25 fs-4 fw-bold">$ {item["producto"]["price"]}</span>
+								</div>
+							</div>
+							<span className="badge bg-primary rounded-pill fs-6">{item["productQauntityCart"]}</span>
+						</li>
+					))}
+			</ol>
+
+			<hr />
+
+			<div className="d-flex flex-row-reverse">
+				<p className="text-uppercase fs-4 fw-bold pe-5">
+					Total <span className="ps-5">$ {totalPriceCart}</span>{" "}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Cart;
--- src/components/cartShopping/ItemCartContainer.js
+++ src/components/cartShopping/ItemCartContainer.js
@@ -1,25 +1,11 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../context/cart/CartContext";
+import Cart from "./Cart";
 
 const ItemCartContainer = () => {
 	const { cartItems } = useContext(CartContext);
 
-	return (
-		<div className="p-5 min-vh-100" style={{ backgroundColor: "#F3F2FC" }}>
-			<h1 className="text-uppercase fs-3 fw-light mb-5"> Carrito </h1>
-			<ol className="list-group">
-				{cartItems.map((item) => (
-					<li className="list-group-item d-flex justify-content-between align-items-start">
-						<div className="ms-2 me-auto">
-							<div className="fw-bold">{item["producto"]["title"]}</div>
-							{item["producto"]["description"]}
-						</div>
-						<span className="badge bg-primary rounded-pill">{item["productQauntityCart"]}</span>
-					</li>
-				))}
-			</ol>
-		</div>
-	);
+	return <Cart cartItems={cartItems} />;
 };
 
 export default ItemCartContainer;
