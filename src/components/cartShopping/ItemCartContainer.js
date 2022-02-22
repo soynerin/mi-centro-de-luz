5787cd727d5bc3cdae04bde3abdd1f553b569d52
soyNerin <neri.agustin.es@outlook.com>
2022-02-21 23:05:23 -0300

    (feat) Mensaje al usuario en el componente del Carrito
    * Se agrega un mensaje para cuando no existen Items en el Carrito de Compras

--- src/components/cartShopping/ItemCartContainer.js
+++ src/components/cartShopping/ItemCartContainer.js
@@ -1,11 +1,34 @@
+import { Button } from "bootstrap";
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../context/cart/CartContext";
 import Cart from "./Cart";
 
 const ItemCartContainer = () => {
 	const { cartItems } = useContext(CartContext);
 
-	return <Cart cartItems={cartItems} />;
+	console.log(cartItems);
+
+	return (
+		<>
+			{cartItems.length > 0 ? (
+				<Cart cartItems={cartItems} />
+			) : (
+				<div className="p-5 min-vh-100" style={{ backgroundColor: "#F3F2FC" }}>
+					<div className="alert alert-primary text-center fs-4" role="alert">
+						No hay nada en el carrito
+					</div>
+					<div className="d-grid gap-2 d-md-flex justify-content-md-end">
+						<Link to="/">
+							<button className="btn btn-outline-primary me-md-2 p-2" type="button">
+								Buscar algo para comprar
+							</button>
+						</Link>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ItemCartContainer;
